be8edf3c7c36b6b56db72a3d30e2bc634c5d7b23
ALUNO <ALUNO@LABHPC06>
2024-02-27 15:06:24 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex_6.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Digite um número inteiro:");
+            a = int.Parse(Console.ReadLine());
+            
+            
+            if (a == (a/4)*4)
+            {
+                Console.WriteLine();
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Número inválido.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
